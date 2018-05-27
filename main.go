e4497a2b64d013de78f33847e4effb4ba6a9709d
Simon Gibbons <simongibbons@gmail.com>
2018-05-27 09:58:53 +0100

    Some terrible code to run the quiz

--- main.go
+++ main.go
@@ -8,14 +8,22 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 type Question struct {
-	question string
-	answer   string
+	question, answer string
 }
 
 type Quiz []Question
+
+type QuizResponse int
+
+const (
+	Correct QuizResponse = iota
+	Incorrect
+	OutOfQuestions
+)
 
 func main() {
 	quiz, err := readQuizFromCSV("problems.csv")
@@ -24,6 +32,54 @@ func main() {
 	}
 
 	fmt.Printf("Got %d questions for quiz\n", len(quiz))
+	quiz.Run(30 * time.Second)
+}
+
+func (q Quiz) Run(duration time.Duration) {
+	timer := time.NewTimer(duration)
+	correctCount := 0
+	incorrectCount := 0
+
+	responses := make(chan QuizResponse)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	go func() {
+		for _, question := range q {
+			fmt.Printf("%s > ", question.question)
+			scanner.Scan()
+			if scanner.Text() == question.answer {
+				responses <- Correct
+			} else {
+				responses <- Incorrect
+			}
+		}
+		responses <- OutOfQuestions
+	}()
+
+label:
+	for {
+		select {
+		case response := <-responses:
+			{
+				if response == Correct {
+					correctCount += 1
+				} else if response == Incorrect {
+					incorrectCount += 1
+				} else if response == OutOfQuestions {
+					break label
+				} else {
+					panic("Unknown response")
+				}
+			}
+		case <-timer.C:
+			fmt.Println("Out of time")
+			break label
+		}
+	}
+
+	totalAsked := correctCount + incorrectCount
+	fmt.Printf("Got (%d/%d) correct.\n", correctCount, totalAsked)
 }
 
 func readQuizFromCSV(filename string) (q Quiz, err error) {
