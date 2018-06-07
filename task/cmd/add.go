a8a86e1bb3dc9ec3b4fa218b4acb035a3b5a1d86
Simon Gibbons <simongibbons@gmail.com>
2018-06-07 08:06:36 +0100

    Support writing tasks

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -1,11 +1,25 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"strings"
+
+	"github.com/simongibbons/gophercises/task/store"
+	"github.com/spf13/cobra"
+)
 
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a task to the TODO list",
+	Use:       "add",
+	Short:     "Add a task to the TODO list",
+	ValidArgs: []string{"task"},
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		db, err := store.New("bolt.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		db.AddTask(store.Task{
+			Content: strings.Join(args, " "),
+		})
 	},
 }
